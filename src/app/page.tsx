33495da5a75e0d78ecab790d1be4f1911c5a0ba3
MiTange <tmrevo210@yahoo.co.jp>
2024-11-19 23:41:07 +0900

    初回デプロイ用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import TetrisGame from "./components/TetrisGame";
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className=" text-xl font-bold flex justify-center">Tetris</h1>
+      <h1 className=" text-2xl font-bold flex justify-center my-2">Tetris</h1>
       <TetrisGame />
     </div>
   )
